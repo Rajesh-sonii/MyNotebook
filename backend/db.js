ad24a29c13b90c43ef5fa8db19574d8a83134d5d
Rajesh Soni <77094212+Rajesh-sonii@users.noreply.github.com>
2025-02-24 18:54:10 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-    const mongoURI = 'mongodb+srv://Rajesh123:Rajesh123@cluster0.afclszb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/myNotebook';
+    const mongoURI = 'mongodb+srv://Rajesh123:Rajesh123@cluster0.afclszb.mongodb.net/myNotebook?retryWrites=true&w=majority&appName=Cluster0/';
   await mongoose.connect(mongoURI);
   mongoose.connection.on('connected', () => console.log('connected'));
 
